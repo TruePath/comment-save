85f3e1316a5c76c03c9fc0a60983bc5b3c054f31
shayanj <shayanj@localhost>
2010-12-11 23:41:20 -0500

    almost have storage of local id working

--- page.js
+++ page.js
@@ -33,9 +33,9 @@ function getTimestamp() {
 	// time
 	var hours = currentTime.getHours()
     var minutes = currentTime.getMinutes()
-    /*if (minutes < 10){
+    if (minutes < 10){
       minutes = "0" + minutes
-    }*/
+    }
     var time = hours + ":" + minutes;
 	
 	// return timestamp
@@ -44,8 +44,19 @@ function getTimestamp() {
   
 // id set or not?
 var idSet = 0;
+var idReset = false;
   
-function sendMessage(){
+function sendMessage(event){
+	alert('Event: ' + event);
+
+	// which key was pressed?
+	var characterCode = event.charCode;
+	alert(event.charCode);
+    if (characterCode == undefined) {
+    	characterCode = event.keyCode;
+    	}
+    alert("The Unicode character code is: " + characterCode);
+
     // get the id - if it already exists good otherwise give it one 
 	
 	// TODO: Assign a new ID from local storage.
@@ -78,7 +89,7 @@ function sendMessage(){
 }
 
 // called when document is loaded - TODO: Should use JQuery instead
-function begin()
+/*function begin()
 {  
    var textareas = document.getElementsByTagName('textarea');  
    var len = textareas.length;
@@ -87,7 +98,7 @@ function begin()
    for(i = 0; i < len; i++) {    
         textareas[i].onkeydown = sendMessage; 
    }
-}
+}*/
 
 // id # in case one is not assigned to the textarea
 var id = 1;
@@ -100,16 +111,68 @@ var port = chrome.extension.connect({name: "comment"});
 
 // Instead of using onload, use JQuery:
 $(document).ready(function(){
-   /*var textareas = document.getElementsByTagName('textarea');  
-   var len = textareas.length;
-   //alert("Length is: " + len);
-   var i;  
-   for(i = 0; i < len; i++) {    
-        textareas[i].onkeydown = sendMessage; 
-   }*/
-   
    // attach function to all textareas
-   $("textarea").live('keypress', sendMessage);
+   $("textarea").live('keypress', function(event) {
+  		//alert('Event: ' + event);
+
+		// which key was pressed?
+		var characterCode = event.charCode;
+    	if (characterCode == undefined) {
+    		characterCode = event.keyCode;
+    	}
+		var actualkey=String.fromCharCode(characterCode);
+   		//alert("Key pressed is: " + actualkey);
+
+    	// get the id - if it already exists good otherwise give it one 
+
+		// GET ID from local storage
+		var requestedID;
+		
+		// request new id if value was empty
+		if (this.value == '') {
+			idSet = 0;
+		}
+		
+		// request id from background.html if not already set
+		if (idSet == 0) {
+			chrome.extension.sendRequest({idRequest: "id"}, function(response) {
+  				requestedID = response.theId;
+				alert("requestedID: " + requestedID);
+				idSet = 1;
+				this.id = requestedID;
+				alert("This id: " + this.id);
+			});
+		}
+		
+		// new ID: involves URL  = "newID" + id++ + document.URL;
+		/*if (this.value == '' || idSet == 0) { // assign new id if textbox is empty 
+			this.id = "newID" + id++ + document.URL;
+			idSet = 1;
+		}*/
+	
+		//if (requestedID) {
+			
+		//}
+	
+		// actual value:
+		var val = this.value + actualkey;
+	
+		// get the timestamp
+		var timestamp = getTimestamp();
+		//alert("Timestamp: " + timestamp + ", Title: " + document.title);
+	
+		// get the title
+		var title1 = document.title;
+		
+		alert("Sending id: " + this.id);
+		
+		// send the message
+		try {
+			port.postMessage({id: this.id, text: val, title: title1, url: document.URL, time: timestamp});
+		} catch(e) {
+			alert("There was an error: " + e);
+		}
+	});
    
    
    //alert('Ran ready');
