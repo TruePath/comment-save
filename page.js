6174bdea7ff68c3a6dee53c9cf7ab5d78e5d0c13
shayanj <shayanj@localhost>
2010-12-29 21:33:57 -0600

    Added context-menu to change tracking

--- page.js
+++ page.js
@@ -1,6 +1,7 @@
 
 // Month array
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
 /* Returns the current time */
 function getTimestamp() {
 	// date
@@ -46,7 +47,6 @@ function getDisqusTitle(url) {
 		var thread = splits[4];
 		// replace all '_' with spaces
 		var t2 = thread.replace("_", " ");
-		//alert("First: " + splits[0] + "\n" + splits[1] + "\n" + splits[2] + "\n" + splits[3]);
 		
 		var title = website + " - " + t2;
 		return title;
@@ -58,7 +58,6 @@ function getDisqusTitle(url) {
 
 }
 
-
 // response function for getting an id
 
 // Instead of using onload, use JQuery:
@@ -67,7 +66,6 @@ $(document).ready(function(){
 
 	// attach function to all textareas
 	$("textarea").live('keyup', function(event) {
-	    //alert("TABINDEX: " + this.tabIndex);
 		// iframe test - FOR DISQUS
 		var iTitle = document.title;
 		var theURL = document.URL; 
@@ -76,8 +74,6 @@ $(document).ready(function(){
 			if (window != window.top) {
 				if (location.hostname.indexOf('.disqus.com') != -1) { // disqus comment check
 					iTitle = getDisqusTitle(theURL);//"Disqus Comment";
-				
-					//alert("theURL: " + theURL + "\nParent: " + parent );
 				}
 				else {
 					iTitle = "Other Comment";
@@ -111,9 +107,7 @@ $(document).ready(function(){
 		var requestedID;
 		
 		// request new id if value was empty [and didnt use backspace or delete]
-		//alert("Length: " + this.value.length);
 		if (this.value.length == 1 && (event.keyCode != 8) && (event.keyCode != 46)) {
-			//alert("Getting new id");
 			idSet = 0;
 		}
 		
@@ -123,9 +117,8 @@ $(document).ready(function(){
 		if (idSet == 0) {
 			chrome.extension.sendRequest({idRequest: "id"}, function(response) {
   				requestedID = response.theId;
-				//alert("requestedID: " + requestedID);
 				idSet = 1;
-				//theTextbox.name = "idSet1";
+				
 				theTextbox.tabIndex = requestedID;
 			});
 		}
@@ -138,22 +131,9 @@ $(document).ready(function(){
 	
 		// actual value:
 		var val = this.value + actualkey;
-		//alert("Value: " + value + ", val: " + val);
 	
 		// get the timestamp
 		var timestamp = getTimestamp();
-		
-		// get the title
-		/*var title1 = document.title;
-		if (title1 == "") {
-			alert("ALERT!! Parent: " + parent);
-			title1 = parent.document.title;
-		}
-		// if title does not exist, try getting hte parent's title
-		//if (!title1)
-		//	title1 = parent.document.title;
-		
-		alert("Title: " + title1);*/
 		
 		// send the message if there is something to send
 		if (val == '')
@@ -165,92 +145,4 @@ $(document).ready(function(){
 		}
 	});
    
-   
-	// attach function to all iframes
-	/*$("iframe").("textarea").live('keyup', function(event) {
-		alert("In the second one!!");
-	
-		// DON'T CHANGE THE NAME
-		var idSet = 0;
-		
-		// try to parse the id into an int
-		var intId = parseInt(this.id);
-		if (isNaN(intId) && this.id) { // FOR SITES LIKE GOOGLE TRANSLATE 
-			return;
-		}
-		else {
-			//alert("IntID is: " + intId);
-			idSet = 1;
-		}
-		
-		// which key was pressed?
-		var characterCode = event.charCode;
-    	if (characterCode == undefined) {
-    		characterCode = event.keyCode;
-    	}
-		
-		//alert('Value on ' + event.type + ': ' + ' KeyCode: ' + event.keyCode);
-		
-		var actualkey=String.fromCharCode(characterCode);
-   		//alert("Key pressed is: " + actualkey);
-
-    	// get the id - if it already exists good otherwise give it one 
-
-		// GET ID from local storage
-		var requestedID;
-		
-		// request new id if value was empty [and didnt use backspace or delete]
-		//alert("Length: " + this.value.length);
-		if (this.value.length == 1 && (event.keyCode != 8) && (event.keyCode != 46)) {
-			//alert("Getting new id");
-			idSet = 0;
-		}
-		
-		var theTextbox = this;
-		
-		// request id from background.html if not already set
-		if (idSet == 0) {
-			chrome.extension.sendRequest({idRequest: "id"}, function(response) {
-  				requestedID = response.theId;
-				//alert("requestedID: " + requestedID);
-				idSet = 1;
-				//theTextbox.name = "idSet1";
-				theTextbox.id = requestedID;
-			});
-		}
-		
-		if (requestedID && idSet == 1) {
-			this.id = requestedID;
-		}
-		else if (idSet == 0)
-			return;
-	
-		// actual value:
-		var val = this.value + actualkey;
-		//alert("Value: " + value + ", val: " + val);
-	
-		// get the timestamp
-		var timestamp = getTimestamp();
-		
-		// get the title
-		/*var title1 = document.title;
-		if (title1 == "") {
-			alert("ALERT!! Parent: " + parent);
-			title1 = parent.document.title;
-		}
-		// if title does not exist, try getting hte parent's title
-		//if (!title1)
-		//	title1 = parent.document.title;
-		
-		alert("Title: " + title1);
-		
-		// send the message if there is something to send
-		if (val == '')
-			return;
-		try {
-			port.postMessage({id: this.id, text: val, title: iTitle, url: document.URL, time: timestamp});
-		} catch(e) {
-			alert("There was an error: " + e);
-		}
-	});*/
  });
