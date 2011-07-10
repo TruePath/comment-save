c712f40fdc0436fa948fb2ed39348adf5cb6b19f
shayanj <shayanj@localhost>
2011-07-10 16:57:45 -0400

    -Preliminary fix for detecting div changes for new Disqus format -Textarea ids are now tracked via a custom dataset. Much better than using tabIndex

--- page.js
+++ page.js
@@ -90,6 +90,149 @@ chrome.extension.onRequest.addListener(
 // Instead of using onload, use JQuery:
 $(document).ready(function(){
 	
+	// check disqus
+	if (location.hostname.indexOf('.disqus.com') != -1) {
+  		// Extract the textarea (there must be exactly one)
+  		var commentBox = document.querySelector('#comment');
+  		
+		if (commentBox) {
+			$('#comment').live('keypress', function() {
+				//var characterCode = event.charCode;
+		
+				//var actualkey=String.fromCharCode(characterCode);
+  				//alert('Yay! ' + this.innerText + actualkey); // Works!
+				
+				// iframe test - FOR DISQUS
+				var iTitle = document.title;
+				var theURL = document.URL; 
+			
+				try {
+					if (window != window.top) {
+						if (location.hostname.indexOf('.disqus.com') != -1) { // disqus comment check
+							iTitle = getDisqusTitle(theURL);//"Disqus Comment";
+						}
+						else {
+							iTitle = "Other Comment";
+						}
+						
+						// the url is the referrer
+						var theURL = document.referrer;
+					}
+				}
+				catch (e) {
+					alert("ERROR: " + e);
+				}
+			
+				// DON'T CHANGE THE NAME
+				var idSet = 0;
+				
+				// try to parse the id into an int
+				var intId = parseInt(this.tabIndex);
+				if (intId != 0) { // 0 is the default value for tab index on most sites
+					idSet = 1;
+				}
+				
+				// which key was pressed?
+				var characterCode = event.charCode;
+				if (characterCode == undefined) {
+					characterCode = event.keyCode;
+				}
+				
+				var actualkey=String.fromCharCode(characterCode);
+
+				// get the id - if it already exists good otherwise give it one 
+
+				// GET ID from local storage
+				var requestedID;
+				//alert('value is: ' + this.value);
+				// request new id if value was empty [and didnt use backspace or delete]
+				if (this.innerText.length == 1 && (event.keyCode != 8) && (event.keyCode != 46)) {
+					idSet = 0;
+					//alert('requesting new id');
+				}
+				
+				// request id from background.html if not already set
+				if (idSet == 0) {
+					chrome.extension.sendRequest({idRequest: "id"}, function(response) {
+						requestedID = response.theId;
+						idSet = 1;
+						
+						this.tabIndex = requestedID;
+					});
+				}
+				
+				if (requestedID && idSet == 1) {
+					this.tabIndex = requestedID;
+				}
+				else if (idSet == 0)
+					return;
+			
+				// actual value:
+				var val = this.value + actualkey;
+				// send the message if there is something to send - charcode check is for facebook 'enter' issue
+				if (val === '' || val === "" || val.length == 0 || val.charCodeAt(0) == 0) {// val.length == 1 
+				
+					//alert("Invalid val");
+					return;
+				}
+				else {
+				//alert("Val2 is: " + val + ", charCode: " + val.charCodeAt(0));
+				
+				// get the timestamp
+				var timestamp = getTimestamp();
+				
+				try {
+					port.postMessage({id: this.tabIndex, text: val, title: iTitle, url: theURL, time: timestamp});
+				} catch(e) {
+					alert("There was an error: " + e);
+				}
+				}
+				
+				
+			});
+    		// Inject some text!
+    		//commentBox.innerText = 'Google Chrome Injected!';
+  		}
+	}
+		
+	//alert('Found formArea: ' + formArea);
+	// check if a disqus textarea exists
+	/*newDisqus = $("#comment");
+	formArea = $("#dsq-form-area");
+	if (formArea.length != 0) {
+		$('iframe').ready(function() {
+			// get all the iframes
+			iframes = document.getElementsByTagName('iframe');
+			alert('iLength: ' + iframes.length);
+			for (var i = 0; i < iframes.length; i++) { 
+				frame = iframes[i];
+				alert('IFrame: ' + frame.id); // Works!!
+				
+   				//newDisqus = i.contents().find('#comment');//$("#comment");
+				dom = frame.contentWindow.document
+				newDisqus = dom.getElementById('comment');
+				alert('NewDisqus: ' + newDisqus.length);
+				if (newDisqus.length != 0) {
+					alert('Found NewDisqus!');
+				}
+  			} 
+			
+			/*for(i in iframes) {
+				alert('Iframe: ' + i.contentDocument);
+				dom = i.contentDocument;
+				newDisqus = dom.getElementById('#comment');
+				//newDisqus = i.contents().find('#comment');//$("#comment");
+				alert('NewDisqus: ' + newDisqus.length);
+				if (newDisqus.length != 0) {
+					alert('Found NewDisqus!');
+				}
+			}*/
+		//});
+		/*alert('NewDisqus ' + formArea.length);//newDisqus.length);
+		newDisqus.change( function(event) {
+			alert('Comment: ' + this.innerHTML);
+		});
+	}*/
 
 	// attach function to all textareas
 	$("textarea").live('keyup', function(event) {
@@ -117,10 +260,13 @@ $(document).ready(function(){
 		// DON'T CHANGE THE NAME
 		var idSet = 0;
 		
-		// try to parse the id into an int
-		var intId = parseInt(this.tabIndex);
-		if (intId != 0) { // 0 is the default value for tab index on most sites
+		// try to get the data attribute
+		var data;
+		try {
+			data = this.getAttribute("data-cs");//this.dataset.cs;//data-cs;
 			idSet = 1;
+		} catch (error) {
+			idSet = 0;
 		}
 		
 		// which key was pressed?
@@ -150,12 +296,14 @@ $(document).ready(function(){
   				requestedID = response.theId;
 				idSet = 1;
 				
-				theTextbox.tabIndex = requestedID;
+				theTextbox.setAttribute("data-cs", requestedID);
 			});
 		}
 		
+		
 		if (requestedID && idSet == 1) {
-			this.tabIndex = requestedID;
+			//this.tabIndex = requestedID;
+			this.setAttribute("data-cs", requestedID);
 		}
 		else if (idSet == 0)
 			return;
@@ -204,8 +352,9 @@ $(document).ready(function(){
 			}
 		}
 		
+		// send the message
 		try {
-			port.postMessage({id: this.tabIndex, text: val, title: iTitle, url: theURL, time: timestamp});
+			port.postMessage({id: this.getAttribute("data-cs"), text: val, title: iTitle, url: theURL, time: timestamp});
 		} catch(e) {
 			alert("There was an error: " + e);
 		}
